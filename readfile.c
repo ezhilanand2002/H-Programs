ad33d87b3bd9b942c901386b51e66fc52691f38a
ezhilanand2002 <168058360+ezhilanand2002@users.noreply.github.com>
2024-08-11 18:39:02 +0530

    readfile.c

---
+++ readfile.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    FILE *file;
+    char ch;
+    file = fopen("example.txt", "w");
+    if (file == NULL) {
+        printf("Error opening file!\n");
+        return 1;
+    }
+    fprintf(file, "Hello, File Handling in C!\n");
+    fclose(file);
+    file = fopen("example.txt", "r");
+    if (file == NULL) {
+        printf("Error opening file!\n");
+        return 1;
+    }
+    while ((ch = fgetc(file)) != EOF) {
+        putchar(ch);
+    }
+    fclose(file);
+
+    return 0;
+}
