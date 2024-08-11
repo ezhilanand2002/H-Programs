af84693f10482ad6065320e1e0423c206222009d
ezhilanand2002 <168058360+ezhilanand2002@users.noreply.github.com>
2024-08-11 18:36:53 +0530

    maximumelement.c

---
+++ maximumelement.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int findMax(int arr[], int size) {
+    int max = arr[0];
+    for (int i = 1; i < size; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+int main() {
+    int arr[] = {10, 20, 5, 30, 25};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Maximum element: %d\n", findMax(arr, size));
+
+    return 0;
+}
